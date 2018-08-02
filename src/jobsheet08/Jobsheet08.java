bc5a04308d2fa70c5fb078d3b07b8b07d6a3f626
ASUS <ASUS@LAPTOP-GUHKRGVP>
2018-08-02 11:16:25 +0700

    For, While, Do While

---
+++ src/jobsheet08/Jobsheet08.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet08;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Jobsheet08 {
+    public static void main(String[] args) {
+        
+        int x;
+        for (x=1;x<=5;x++)
+        {
+            System.out.println("SMK Telkom Malang");
+        }
+        
+        int y = 1;
+        while (y <= 5)
+        {
+            System.out.println("Smk telkom malang");
+            y++;
+        }
+        
+        int z = 1;
+        do
+        {
+            System.out.println("SMK Telkom Malang");
+            z++;
+        }
+        while (z <= 5);
+        
+    }
+}
